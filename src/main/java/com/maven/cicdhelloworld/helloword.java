fded59137c8309f647d9c72088c474ff6f145fe0
Amol  Johare <amol.johare@acc.ltd>
2022-05-09 12:39:52 +0530

    first commit

---
+++ src/main/java/com/maven/cicdhelloworld/helloword.java
@@ -0,0 +1,15 @@
+package com.maven.cicdhelloworld;
+
+import javax.naming.Context;
+
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class helloword implements RequestHandler<Object, String> {
+
+	@Override
+	public String handleRequest(Object input, com.amazonaws.services.lambda.runtime.Context context) {
+		System.out.println("hello world");
+		return "hello word";
+	
+	}
+}
