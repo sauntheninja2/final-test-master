fa2c3081d908a6ec80c1070307b67e8223afea5d
Amol Johare <64434724+amoljohare@users.noreply.github.com>
2022-05-24 16:05:54 +0530

    Update helloword.java

--- src/main/java/com/maven/cicdhelloworld/helloword.java
+++ src/main/java/com/maven/cicdhelloworld/helloword.java
@@ -8,7 +8,7 @@ public class helloword implements RequestHandler<Object, String> {
 
 	@Override
 	public String handleRequest(Object input, com.amazonaws.services.lambda.runtime.Context context) {
-		System.out.println("hello world2");
+		System.out.println("hello word");
 		return "hello word";
 	
 	}
